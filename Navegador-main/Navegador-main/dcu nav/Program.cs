ac7aeadede71c99fc3536feb26e0593ac101fc1c
RosannaM1 <90869698+RosannaM1@users.noreply.github.com>
2022-03-14 10:13:43 -0400

    Add files via upload

---
+++ Navegador-main/Navegador-main/dcu nav/Program.cs
@@ -0,0 +1,37 @@
+﻿using EasyTabs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mondongo_Browser
+{
+    static class Program
+    {
+       
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            AppContainer container = new AppContainer();
+
+            container.Tabs.Add(
+               new TitleBarTab(container)
+               {
+                   Content = new DCUnavegador
+                   {
+¿                   }
+               }
+
+            );
+            container.SelectedTabIndex = 0;
+            TitleBarTabsApplicationContext applicationContext = new TitleBarTabsApplicationContext();
+            applicationContext.Start(container);
+            Application.Run(applicationContext);
+        }
+    }
+}
